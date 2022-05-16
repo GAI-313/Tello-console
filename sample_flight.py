be26c1eb0787b0507cf1329d28d363d4d02df5e9
GAI <67356536+GAI-313@users.noreply.github.com>
2022-05-16 16:19:25 +0900

    Update sample_flight.py

--- sample_flight.py
+++ sample_flight.py
@@ -4,17 +4,17 @@
 from tello import console #tello.py の console クラスを呼び出す。
 
 def main(): # メイン関数
-    drone = console() # console クラスを有効化
-    drone.takeoff() # 離陸
-    drone.cw(360) # 時計回りに 360°旋回
-    drone.down(50) # 50 cm 下降
-    drone.up(100) # 100 cm 上昇
-    drone.forward(100)  # 100 cm 前進
-    drone.back(100) # 100 cm 後進
-    drone.cw(90) # 時計回りに 90°旋回
-    drone.left(100)  # 100 cm 左
-    drone.right(100) # 100 cm 右
-    drone.land() # 着陸
+    try: # 例外処理　except と一緒に記述する
+        drone = console() # console クラスを有効化
+        
+        drone.takeoff() # 離陸
+        drone.cw(360) # 360°旋回
+        drone.land() # 着陸
+    
+    except KeyboadInterput: # Cntrol + C が押されたら以下の処理を実行
+        drone.emergency() # モーター停止
+    
+    del drone
 
 if __name__ == "__main__": # このプログラムが外部から実行されないようにするおまじない
     main() # メインを実行
